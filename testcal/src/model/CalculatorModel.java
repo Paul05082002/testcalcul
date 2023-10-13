f1c0ee838a7fcd77a9d3046a11ea91f7dff70689
pauldumas <pauldumas@localhost>
2023-10-13 14:23:53 +0200

    Test 2

---
+++ testcal/src/model/CalculatorModel.java
@@ -0,0 +1,8 @@
+package model;
+import java.util.ArrayList;
+
+public class CalculatorModel {
+	public ArrayList<Integer> tab;
+
+
+}
--- testcal/src/model/CalculatorModelInterface.java
+++ testcal/src/model/CalculatorModelInterface.java
@@ -1,6 +1,6 @@
-package testcal;
+package model;
 
-public class test {
+public class CalculatorModelInterface {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
