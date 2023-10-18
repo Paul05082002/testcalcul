7307f21f2f7484c9f154854abac1ddc299b5a0d9
Kewin <Kewin@159.31.79.123>
2023-10-18 13:54:56 +0200

    bjr# WARNING: head commit changed in the meantime
    c

---
+++ testcal/src/view/Window.java
@@ -0,0 +1,14 @@
+package view;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Window extends Application {
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
