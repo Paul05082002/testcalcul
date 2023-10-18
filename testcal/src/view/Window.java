3cb21801c90e889eac77c7e0e4dbf6c507d08159
Kewin <Kewin@159.31.70.220>
2023-10-18 14:01:56 +0200

    création boutons dans fenêtre

--- testcal/src/view/Window.java
+++ testcal/src/view/Window.java
@@ -8,7 +8,21 @@ public class Window extends Application {
 	@Override
 	public void start(Stage arg0) throws Exception {
 		// TODO Auto-generated method stub
+		Button bouton1 = new Button("1");
+		Button bouton2 = new Button("2");
+		Button bouton3 = new Button("3");
+		Button bouton4 = new Button("4");
+		Button bouton5 = new Button("5");
+		Button bouton6 = new Button("6");
+		Button bouton7 = new Button("7");
+		Button bouton8 = new Button("8");
+		Button bouton9 = new Button("9");
 		
+		Button boutonP = new Button("+");
+		Button boutonM = new Button("-");
+		Button boutonF = new Button("X");
+		Button boutonD = new Button("/");
+		Button bouton_Push = new Button("<>");
 	}
 
 }
